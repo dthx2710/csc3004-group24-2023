2a503f6cc24ab1771dd441c2924a9a12da983346
Kai En Yong <kaien.yong99@gmail.com>
2023-05-27 01:42:15 +0800

    Added Ongoing/Ended Polls toggle button

--- frontend/src/UserHomePage.jsx
+++ frontend/src/UserHomePage.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Container, Typography, Button, ButtonGroup } from '@mui/material';
 import PollList from './PollList';
 
 export default function UserHomePage() {
+  const [pollStatus, setPollStatus] = useState('ongoing');
+
   const polls = [
-    { title: 'Ang Mo Kio GRC - GE2020', description: 'Description for poll 1', isCompulsory: false },
-    { title: 'Poll 2', description: 'Description for poll 2', isCompulsory: true },
+    { title: 'Ang Mo Kio GRC - GE2020', description: 'Description for poll 1', isCompulsory: false, status: 'ongoing' },
+    { title: 'Poll 2', description: 'Description for poll 2', isCompulsory: true, status: 'ended' },
     // other polls...
   ];
+
+  const filteredPolls = polls.filter(poll => poll.status === pollStatus);
+
+  const selectedButtonStyle = { backgroundColor: '#3f51b5', color: 'white' };
+  const unselectedButtonStyle = { backgroundColor: '#ABB0B8' };
 
   return (
     <div style={{backgroundColor: 'white'}}>
         <Container maxWidth="md">
           <Box sx={{ mt: 4, mb: 2 }}>
             <Typography variant="h4" component="h1" gutterBottom style={{ color: 'black'}}>
-              Available Polls
+              {pollStatus.charAt(0).toUpperCase() + pollStatus.slice(1)} Polls
             </Typography>
-            <PollList polls={polls} />
+            <Box sx={{ my: 2 }}>
+              <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                <Button style={pollStatus === 'ongoing' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setPollStatus('ongoing')}>Ongoing</Button>
+                <Button style={pollStatus === 'ended' ? selectedButtonStyle : unselectedButtonStyle} onClick={() => setPollStatus('ended')}>Ended</Button>
+              </ButtonGroup>
+            </Box>
+            <PollList polls={filteredPolls} />
           </Box>
         </Container>
     </div>
