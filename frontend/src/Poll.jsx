d6cbc223140875a375f362fe12c88d3523cfdfea
XiangHui <92633730+XiangHui556@users.noreply.github.com>
2023-06-13 04:17:07 +0800

    Revert "To update the poll list after a poll has been deleted"
    This reverts commit 96dc447dcf863dfa626c62318eb2b496a6d78b43.

--- frontend/src/Poll.jsx
+++ frontend/src/Poll.jsx
@@ -3,25 +3,44 @@ import BarChartIcon from '@mui/icons-material/BarChart';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import axios from "axios";
 
-function Poll({ poll, isAdmin, deletePoll }) {
+export default function Poll({ poll, isAdmin, state }) {
   Poll.propTypes = {
     poll: PropTypes.object.isRequired,
     isAdmin: PropTypes.bool.isRequired,
-    deletePoll: PropTypes.func.isRequired,
   };
+
+  const { data } = state;
+  const { pollId } = data;
 
   const handleDeleteClick = (e) => {
     e.stopPropagation();
     if (window.confirm("Do you really want to delete this poll?")) {
-      deletePoll(poll.pollId);
+      deletePoll();
+      console.log("Poll deleted");
     }
   };
+
+  function deletePoll() {
+      axios
+      .delete(`/api/polls/${pollId}`)
+      .then((response) => {
+          if (response.status === 200) {
+              console.log(response);
+          }
+          })
+          .catch((error) => {
+          console.log(error);
+          }); 
+
+    console.log('Button clicked!');
+  }
 
   return (
     <Paper style={{ padding: '16px', backgroundColor: '#f5f5f5', width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>
-        <Link to={`/pollvoteform/${poll.title}/${poll.description}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Link to={`/pollvoteform/${poll.title}/${poll.description}`} style={{ textDecoration: 'none', color: 'inherit' }} state={{ data: { pollId: poll.pollId }}}>
           <Typography variant="h5">{poll.title}</Typography>
           <Typography variant="body1">{poll.description}</Typography>
         </Link>
@@ -38,6 +57,4 @@ function Poll({ poll, isAdmin, deletePoll }) {
       )}
     </Paper>
   );
-}
-
-export default Poll;
+};
